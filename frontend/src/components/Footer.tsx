d9423394cd36cf78fe56003e9e66f4c97e7d5ff7
Johan Santana Galvan <johanjaviersantanagalvan@alumno.ieselrincon.es>
2023-09-14 18:55:26 +0100

    doing footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,13 +1,21 @@
 
 import '../styles/Footer.scss';
+import { AiOutlineHome } from "react-icons/ai";
+import { AiOutlinePlus } from "react-icons/ai";
+import { AiOutlineSearch } from "react-icons/ai";
+
 
 
 function Footer() {
     return (
-      <div className="footer">
-
-      </div>
+        <div className="footer-container">
+            <div className='footer-icons'>
+                <h1><AiOutlineHome></AiOutlineHome></h1>
+                <h1><AiOutlinePlus></AiOutlinePlus></h1>
+                <h1><AiOutlineSearch></AiOutlineSearch></h1>
+            </div>
+        </div>
     );
-  }
+}
 
-  export default Footer;
+export default Footer;
