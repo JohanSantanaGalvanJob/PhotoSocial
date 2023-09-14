4cca4bcdc3bea53a2d5461ea956582ff770a7b7e
Johan Santana Galvan <johanjaviersantanagalvan@alumno.ieselrincon.es>
2023-09-14 17:56:41 +0100

    fixed folders

--- backend/src/test/java/com/example/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/example/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.eventcalendar;
+package com.example.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EventCalendarApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
